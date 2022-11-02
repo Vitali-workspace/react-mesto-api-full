a28f4fa55e72515a8c50072fe2cb233601547660
vitali <vitali.slepnyov@gmail.com>
2022-11-02 10:07:57 +0300

    fix: исправить удаление карточки

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -146,7 +146,7 @@ function App() {
   function handleCardDelete(card) {
     api.deleteCardServer(card)
       .then(() => {
-        const newListCards = cards.filter((elementCardId) => elementCardId === card ? false : true)
+        const newListCards = cards.filter((elementCard) => elementCard.id === card ? false : true)
         setCards(newListCards);
         closeAllPopups();
       })
